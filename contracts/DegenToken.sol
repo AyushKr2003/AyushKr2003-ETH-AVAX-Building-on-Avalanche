6e143ae5f9c94fb6088b2c7f655c81e296801677
AyushKr <ayushkrsngh2003@gmail.com>
2024-07-20 09:50:04 +0530

    Reedem function update

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
-    string items = "1.DEGEN NFT  2.DEGEN T-shirt 3.DEGEN Treasure Chest";
+    string items = "1.DEGEN NFT:10DGN  2.DEGEN T-shirt:50DGN  3.DEGEN Treasure Chest:100DGN";
 
     mapping(address => uint) public nftBalance;
     mapping(address => uint) public tshirtBalance;
@@ -22,23 +22,23 @@ contract DegenToken is ERC20, Ownable {
     }
 
     function RedeemToken(uint choice) public {
-    require(choice >= 1 && choice <= 3, "Invalid choice. Choose an item from the list");
+        require(choice >= 1 && choice <= 3, "Invalid choice. Choose an item from the list");
 
-    if (choice == 1) {
-        require(balanceOf(msg.sender) >= 10, "Insufficient balance to redeem NFT");
-        _burn(msg.sender, 10);
-        nftBalance[msg.sender] += 1;  
+        if (choice == 1) {
+            require(balanceOf(msg.sender) >= 10, "Insufficient balance to redeem NFT");
+            _burn(msg.sender, 10);
+            nftBalance[msg.sender] += 1;  
+            }
+        else if (choice == 2) {
+            require(balanceOf(msg.sender) >= 50, "Insufficient balance to redeem T-shirt");
+            _burn(msg.sender, 50);
+            tshirtBalance[msg.sender] += 1;  
         }
-    else if (choice == 2) {
-        require(balanceOf(msg.sender) >= 50, "Insufficient balance to redeem T-shirt");
-        _burn(msg.sender, 50);
-        tshirtBalance[msg.sender] += 1;  
-    }
-    else if (choice == 3) {
-        require(balanceOf(msg.sender) >= 100, "Insufficient balance to redeem Treasure Chest");
-        _burn(msg.sender, 100);
-        chestBalance[msg.sender] += 1;  
-    }
+        else if (choice == 3) {
+            require(balanceOf(msg.sender) >= 100, "Insufficient balance to redeem Treasure Chest");
+            _burn(msg.sender, 100);
+            chestBalance[msg.sender] += 1;  
+        }
     }
 
     function burn(uint _value) public {
@@ -46,9 +46,9 @@ contract DegenToken is ERC20, Ownable {
     }
 
     function burnFrom(address _from, uint256 _value) public {
-    address spender = _msgSender();
-    _spendAllowance(_from, spender, _value);
-    _burn(_from, _value);
+        address spender = _msgSender();
+        _spendAllowance(_from, spender, _value);
+        _burn(_from, _value);
     }
 
     function showItems() external view returns(string memory){
